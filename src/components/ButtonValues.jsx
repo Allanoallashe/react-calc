0661acc1f07b4a7bc7221704547cbf2873379e99
allan lenny <owinoallan77@gmail.com>
2023-08-22 19:21:03 +0300

    ^

--- src/components/ButtonValues.jsx
+++ src/components/ButtonValues.jsx
@@ -12,6 +12,8 @@ const ButtonValues = ({ setDisplayValue, displayValue }) => {
 
   const [keyBuffer, setKeyBuffer] = useState('');
   const [lastKeyPressed, setLastKeyPressed] = useState('');
+  const [exponentBuffer, setExponentBuffer] = useState(null); 
+
 
 const handleKeyDown = (event) => {
   const key = event.key;
@@ -19,9 +21,12 @@ const handleKeyDown = (event) => {
   if (/^[0-9]$/.test(key)) {
     event.preventDefault();
     handleButtonClick(key);
-  } else if (/^[+\-*/^.]$/.test(key)) {
+  } else if (/^[+\-*/.^]$/.test(key)) {
     event.preventDefault();
-    if (!isNaN(displayValue.slice(-1))) {
+    if (key === '^') {
+      setKeyBuffer(''); // Clear keyBuffer for exponent input
+      handleButtonClick('^'); // Handle '^' separately
+    } else if (!isNaN(displayValue.slice(-1))) {
       handleButtonClick(key);
     }
   } else if (key === 'Enter') {
@@ -46,17 +51,29 @@ const handleKeyDown = (event) => {
       document.removeEventListener('keydown', handleKeyDown);
     };
   }, [keyBuffer]);
-  
+
+
 const handleButtonClick = (value) => {
   if (value === '=') {
     try {
-      const result = new Function('return ' + displayValue)();
+      let result = displayValue;
+      if (exponentBuffer !== null) {
+        const base = parseFloat(exponentBuffer);
+        const exponent = parseFloat(displayValue);
+        result = Math.pow(base, exponent);
+      } else {
+        result = new Function('return ' + displayValue)();
+      }
       setDisplayValue(formatResult(result));
     } catch (err) {
       setDisplayValue('Error');
     }
+    setKeyBuffer('');
+    setExponentBuffer(null); // Reset exponentBuffer
   } else if (value === 'C') {
     setDisplayValue('0');
+    setKeyBuffer('');
+    setExponentBuffer(null); // Reset exponentBuffer
   } else if (['sin', 'cos', 'tan'].includes(value)) {
     const radians = (Math.PI / 180) * parseFloat(displayValue);
     let result;
@@ -64,24 +81,36 @@ const handleButtonClick = (value) => {
     else if (value === 'cos') result = Math.cos(radians);
     else if (value === 'tan') result = Math.tan(radians);
     setDisplayValue(formatResult(result));
-  } else if (value === 'log') {
-    setDisplayValue(formatResult(Math.log10(parseFloat(displayValue))));
-  } else if (value === 'sqrt') {
-    setDisplayValue(formatResult(Math.sqrt(parseFloat(displayValue))));
-  } else if (value === '^') {
-    if (!isNaN(displayValue) && !isNaN(keyBuffer)) {
-      const result = Math.pow(parseFloat(displayValue), parseFloat(keyBuffer));
-      setDisplayValue(formatResult(result));
-    }
     setKeyBuffer('');
+    setExponentBuffer(null); // Reset exponentBuffer
+    setLastKeyPressed('='); // Reset lastKeyPressed when trigonometric button is clicked
+  } else if (value === 'log') {
+    const result = Math.log10(parseFloat(displayValue));
+    setDisplayValue(formatResult(result));
+    setKeyBuffer('');
+    setExponentBuffer(null); // Reset exponentBuffer
+  } else if (value === 'sqrt') {
+    const result = Math.sqrt(parseFloat(displayValue));
+    setDisplayValue(formatResult(result));
+    setKeyBuffer('');
+    setExponentBuffer(null); // Reset exponentBuffer
+  } else if (value === '^') {
+    if (!isNaN(displayValue)) {
+      setExponentBuffer(displayValue); // Capture the base value
+      setDisplayValue(displayValue + '^'); // Display the base with '^'
+      setKeyBuffer('');
+      setLastKeyPressed('^'); // Set lastKeyPressed to '^'
+    }
   } else if (['+', '-', '*', '/'].includes(value)) {
     if (!isNaN(keyBuffer) && !isNaN(displayValue.slice(-1))) {
       setDisplayValue(displayValue + value);
     }
   } else if (value === 'OFF') {
     setDisplayValue('');
+    setKeyBuffer('');
+    setExponentBuffer(null); // Reset exponentBuffer
   } else {
-    if (displayValue === '0' || displayValue === 'Error' || lastKeyPressed === '=') {
+    if (displayValue === '0' || displayValue === 'Error' || lastKeyPressed === '=' || ['sin', 'cos', 'tan'].includes(lastKeyPressed)) {
       setDisplayValue(value);
     } else if (value === '.' && displayValue.includes('.')) {
       return;
@@ -91,15 +120,19 @@ const handleButtonClick = (value) => {
   }
 
   if (/^[0-9]$/.test(value)) {
-    setKeyBuffer(keyBuffer + value); // Add numeric key to buffer
+    setKeyBuffer(keyBuffer + value);
+    if (exponentBuffer !== null) {
+      setExponentBuffer(exponentBuffer + value);
+    }
+  } else if (value === '^') {
+    setKeyBuffer('');
   } else {
-    setKeyBuffer(''); // Clear the buffer for non-numeric keys
+    setKeyBuffer('');
+    setExponentBuffer(null);
   }
-  
-  setLastKeyPressed(value); // Update the last key pressed
+
+  setLastKeyPressed(value);
 };
-
-
 
 
 
