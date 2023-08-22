789963582ee702456ae00a5fa1971f9c47d3bea3
allan lenny <owinoallan77@gmail.com>
2023-08-22 14:15:08 +0300

    working buttons

--- src/components/ButtonValues.jsx
+++ src/components/ButtonValues.jsx
@@ -8,61 +8,48 @@ const btnValues = [
   '0', '.', '+', '=',
   ]
 
-const ButtonValues = ({setDisplayValue,displayValue}) => {
-console.log({displayValue})
+const ButtonValues = ({ setDisplayValue, displayValue }) => {
+  
 const handleButtonClick = (value) => {
   if (value === '=') {
     try {
-      const safeEval = new Function('return ' + displayValue);
-      const result = safeEval();
+      const result = new Function('return ' + displayValue)();
       setDisplayValue(formatResult(result));
     } catch (err) {
       setDisplayValue('Error');
     }
   } else if (value === 'C') {
     setDisplayValue('0');
-  } else if (value === 'sin' || value === 'cos' || value === 'tan') {
+  } else if (['sin', 'cos', 'tan'].includes(value)) {
     const radians = (Math.PI / 180) * parseFloat(displayValue);
     let result;
-    if (value === 'sin') {
-      result = Math.sin(radians);
-    } else if (value === 'cos') {
-      result = Math.cos(radians);
-    } else if (value === 'tan') {
-      result = Math.tan(radians);
-    }
+    if (value === 'sin') result = Math.sin(radians);
+    else if (value === 'cos') result = Math.cos(radians);
+    else if (value === 'tan') result = Math.tan(radians);
     setDisplayValue(formatResult(result));
   } else if (value === 'log') {
-    const result = Math.log10(parseFloat(displayValue));
-    setDisplayValue(formatResult(result));
+    setDisplayValue(formatResult(Math.log10(parseFloat(displayValue))));
   } else if (value === 'sqrt') {
-    const result = Math.sqrt(parseFloat(displayValue));
-    setDisplayValue(formatResult(result));
+    setDisplayValue(formatResult(Math.sqrt(parseFloat(displayValue))));
   } else if (value === '^') {
-    setDisplayValue(displayValue + '^' );
-  }
-  else if (value === '+' || value === '-' || value === '*' || value === '/') {
-    if (displayValue === '0' || displayValue === 'Error') {
-      setDisplayValue(value );
-    } else if (displayValue[displayValue.length - 1] === '.' || isNaN(displayValue[displayValue.length - 1])) {
-      setDisplayValue(displayValue.slice(0, -1) + value);
-    } else {
-      setDisplayValue(formatResult(displayValue + value));
+    setDisplayValue(displayValue + '^');
+  } else if (['+', '-', '*', '/'].includes(value)) {
+    if (!isNaN(displayValue.slice(-1))) {
+      setDisplayValue(displayValue + value);
     }
-  }
-  else if (value === 'OFF') {
-    setDisplayValue('')
-  }
-  else {
+  } else if (value === 'OFF') {
+    setDisplayValue('');
+  } else {
     if (displayValue === '0' || displayValue === 'Error') {
       setDisplayValue(value);
     } else if (value === '.' && displayValue.includes('.')) {
       return;
     } else {
-      setDisplayValue(formatResult(displayValue + value));
+      setDisplayValue(displayValue + value);
     }
   }
 };
+
 
 
 const formatResult = (value) => {
