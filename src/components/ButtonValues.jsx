b98184cfb08a59c89e7ed64a857091fd0f0b23c0
allan lenny <owinoallan77@gmail.com>
2023-08-31 22:35:37 +0300

    decimals

--- src/components/ButtonValues.jsx
+++ src/components/ButtonValues.jsx
@@ -6,7 +6,7 @@ const btnValues = [
   '4', '5', '6', '*',
   '1', '2', '3', '-',
   '0', '.', '+', '=',
-  ]
+]
 
 const ButtonValues = ({ setDisplayValue, displayValue }) => {
 
@@ -25,7 +25,7 @@ const handleKeyDown = (event) => {
     event.preventDefault();
     if (key === '^') {
       setKeyBuffer(''); // Clear keyBuffer for exponent input
-      handleButtonClick('^'); // Handle '^' separately
+      handleButtonClick('^');
     } else if (!isNaN(displayValue.slice(-1))) {
       handleButtonClick(key);
     }
@@ -39,100 +39,16 @@ const handleKeyDown = (event) => {
       setKeyBuffer(keyBuffer.slice(0, -1)); // Remove the last character from keyBuffer
     }
   }
-
-  // Don't set the keyBuffer here
 };
 
   
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
-    // Clean up the event listener on component unmount
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
   }, [keyBuffer]);
 
-
-// const handleButtonClick = (value) => {
-//   if (value === '=') {
-//     try {
-//       let result = displayValue;
-//       if (exponentBuffer !== null) {
-//         const base = parseFloat(exponentBuffer);
-//         const exponent = parseFloat(displayValue);
-//         result = Math.pow(base, exponent);
-//       } else {
-//         result = new Function('return ' + displayValue)();
-//       }
-//       setDisplayValue(formatResult(result));
-//     } catch (err) {
-//       setDisplayValue('Error');
-//     }
-//     setKeyBuffer('');
-//     setExponentBuffer(null); // Reset exponentBuffer
-//   } else if (value === 'C') {
-//     setDisplayValue('0');
-//     setKeyBuffer('');
-//     setExponentBuffer(null); // Reset exponentBuffer
-//   } else if (['sin', 'cos', 'tan'].includes(value)) {
-//     const radians = (Math.PI / 180) * parseFloat(displayValue);
-//     let result;
-//     if (value === 'sin') result = Math.sin(radians);
-//     else if (value === 'cos') result = Math.cos(radians);
-//     else if (value === 'tan') result = Math.tan(radians);
-//     setDisplayValue(formatResult(result));
-//     setKeyBuffer('');
-//     setExponentBuffer(null); // Reset exponentBuffer
-//     setLastKeyPressed('='); // Reset lastKeyPressed when trigonometric button is clicked
-//   } else if (value === 'log') {
-//     const result = Math.log10(parseFloat(displayValue));
-//     setDisplayValue(formatResult(result));
-//     setKeyBuffer('');
-//     setExponentBuffer(null); // Reset exponentBuffer
-//   } else if (value === 'sqrt') {
-//     const result = Math.sqrt(parseFloat(displayValue));
-//     setDisplayValue(formatResult(result));
-//     setKeyBuffer('');
-//     setExponentBuffer(null); // Reset exponentBuffer
-//   } else if (value === '^') {
-//     if (!isNaN(displayValue)) {
-//       setExponentBuffer(displayValue); // Capture the base value
-//       setDisplayValue(displayValue + '^'); // Display the base with '^'
-//       setKeyBuffer('');
-//       setLastKeyPressed('^'); // Set lastKeyPressed to '^'
-//     }
-//   } else if (['+', '-', '*', '/'].includes(value)) {
-//     if (!isNaN(keyBuffer) && !isNaN(displayValue.slice(-1))) {
-//       setDisplayValue(displayValue + value);
-//     }
-//   } else if (value === 'OFF') {
-//     setDisplayValue('');
-//     setKeyBuffer('');
-//     setExponentBuffer(null); // Reset exponentBuffer
-//   } else {
-//     if (displayValue === '0' || displayValue === 'Error' || lastKeyPressed === '=' || ['sin', 'cos', 'tan'].includes(lastKeyPressed)) {
-//       setDisplayValue(value);
-//     } else if (value === '.' && displayValue.includes('.')) {
-//       return;
-//     } else {
-//       setDisplayValue(displayValue + value);
-//     }
-//   }
-
-//   if (/^[0-9]$/.test(value)) {
-//     setKeyBuffer(keyBuffer + value);
-//     if (exponentBuffer !== null) {
-//       setExponentBuffer(exponentBuffer + value);
-//     }
-//   } else if (value === '^') {
-//     setKeyBuffer('');
-//   } else {
-//     setKeyBuffer('');
-//     setExponentBuffer(null);
-//   }
-
-//   setLastKeyPressed(value);
-// };
   
 const handleButtonClick = (value) => {
   if (value === '=') {
@@ -142,7 +58,6 @@ const handleButtonClick = (value) => {
         const base = parseFloat(exponentBuffer);
         const exponent = parseFloat(displayValue);
         result = Math.pow(base, exponent);
-        setExponentBuffer(null); // Reset exponentBuffer
       } else {
         result = new Function('return ' + displayValue)();
       }
@@ -151,6 +66,7 @@ const handleButtonClick = (value) => {
       setDisplayValue('Error');
     }
     setKeyBuffer('');
+    setExponentBuffer(null); // Reset exponentBuffer
   } else if (value === 'C') {
     setDisplayValue('0');
     setKeyBuffer('');
@@ -163,24 +79,24 @@ const handleButtonClick = (value) => {
     else if (value === 'tan') result = Math.tan(radians);
     setDisplayValue(formatResult(result));
     setKeyBuffer('');
-    setExponentBuffer(null); // Reset exponentBuffer
-    setLastKeyPressed('='); // Reset lastKeyPressed when trigonometric button is clicked
+    setExponentBuffer(null)
+    setLastKeyPressed('='); 
   } else if (value === 'log') {
     const result = Math.log10(parseFloat(displayValue));
     setDisplayValue(formatResult(result));
     setKeyBuffer('');
-    setExponentBuffer(null); // Reset exponentBuffer
+    setExponentBuffer(null); 
   } else if (value === 'sqrt') {
     const result = Math.sqrt(parseFloat(displayValue));
     setDisplayValue(formatResult(result));
     setKeyBuffer('');
-    setExponentBuffer(null); // Reset exponentBuffer
+    setExponentBuffer(null); 
   } else if (value === '^') {
     if (!isNaN(displayValue)) {
-      setExponentBuffer(displayValue); // Capture the base value
+      setExponentBuffer(displayValue);
       setDisplayValue(displayValue + '^'); // Display the base with '^'
       setKeyBuffer('');
-      setLastKeyPressed('^'); // Set lastKeyPressed to '^'
+      setLastKeyPressed('^'); 
     }
   } else if (['+', '-', '*', '/'].includes(value)) {
     if (!isNaN(keyBuffer) && !isNaN(displayValue.slice(-1))) {
@@ -189,7 +105,7 @@ const handleButtonClick = (value) => {
   } else if (value === 'OFF') {
     setDisplayValue('');
     setKeyBuffer('');
-    setExponentBuffer(null); // Reset exponentBuffer
+    setExponentBuffer(null); 
   } else {
     if (displayValue === '0' || displayValue === 'Error' || lastKeyPressed === '=' || ['sin', 'cos', 'tan'].includes(lastKeyPressed)) {
       setDisplayValue(value);
@@ -213,13 +129,31 @@ const handleButtonClick = (value) => {
 };
 
 
-const formatResult = (value) => {
+// const formatResult = (value) => {
+//   const roundedValue = parseFloat(value).toFixed(8);
+//   const parts = roundedValue.split('.');
+//   if (parts[1] === '00000000') {
+//     return parts[0]; // Remove decimal point if all digits are zero
+//   }
+//   return roundedValue;
+// };
+  
+ const formatResult = (value) => {
   const roundedValue = parseFloat(value).toFixed(8);
   const parts = roundedValue.split('.');
-  if (parts[1] === '00000000') {
-    return parts[0]; // Remove decimal point if all digits are zero
+  
+  if (parts[1]) {
+    let decimalPart = parts[1];
+    let endIndex = decimalPart.length - 1;
+
+    while (decimalPart[endIndex] === '0') {
+      endIndex--;
+    }
+
+    parts[1] = decimalPart.substring(0, endIndex + 1);
   }
-  return roundedValue;
+  
+  return parts.join('.');
 };
 
 
